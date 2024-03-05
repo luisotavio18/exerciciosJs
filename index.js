088b049f70d6c5b9453dd7cd7291bc4f97f0f34d
LUIS OTAVIO ZAMBRZYCKI <03979057003@senacrs.edu.br>
2024-03-05 11:32:48 -0300

    alteracoes

---
+++ index.js
@@ -0,0 +1,15 @@
+const num1 = 20;
+const num2 = 654;
+
+let soma = num1 + num2;
+let divisao = num1 / num2;
+let multiplicacao = num1 * num2;
+let subtracao = num1 - num2;
+
+console.log("O valor da soma é " + soma + " - daleeeeeeeee gremio");
+
+console.log("O valor da soma é " + divisao + " - daleeeeeeeee gremio");
+
+console.log("O valor da soma é " + multiplicacao + " - daleeeeeeeee gremio");
+
+console.log("O valor da soma é " + subtracao + " - daleeeeeeeee gremio");
