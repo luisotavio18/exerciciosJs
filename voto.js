1706c498854d87a82d19e7d80adebd47247f2d05
LUIS OTAVIO ZAMBRZYCKI <03979057003@senacrs.edu.br>
2024-03-08 11:29:45 -0300

    voto exercicio

---
+++ voto.js
@@ -0,0 +1,9 @@
+const idade = 18;
+
+if (idade >= 16 && idade <= 17) {
+    console.log('Facultativo')
+}   else if (idade < 16){
+    console.log('Não pode votar')
+}   else if(idade > 18){
+    console.log('Obrigatório')
+}
